96a7bacf79ebe163fe1a1c7109682b63de1a1e75
dropcameron <35440160+dropcameron@users.noreply.github.com>
2018-01-22 16:13:05 +1100

    Added angles
    Uses quivers with 1,1 arrows to allow us to select the angle

--- mapping.py
+++ mapping.py
@@ -2,33 +2,49 @@
 import os
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
+from math import degrees
 from scipy.misc import imread
+import numpy as np
 __location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    # Sets the directory to where the current file is being run from
+   os.path.join(os.getcwd(), os.path.dirname(__file__)))
+   # Sets the directory to where the current file is being run from
+fig, ax = plt.subplots()
 
 DATAFILE = cbook.get_sample_data(os.path.join(__location__, 'g152972.png'))
 IMAGE = imread(DATAFILE)
 plt.imshow(IMAGE, extent=[0, 1646, -411.5, 411.5])
 
 # Enter co-ordinates here
-POINTS = [[1, 2], [3, 4]]
+POINTS = [[0, -300, 1.5708], [800, -225,3], [610, -250,43], [525, -180,22], [750, -200,43]]
 
 X_VALUE = []
 Y_VALUE = []
+PHI=[]
+
+
+
+
+#phi = [2,3,4,2,43]
+
 POINT_NUMBER = []
 for i in range(len(POINTS)):
-    X_VALUE.append(POINTS[i][0])
-    Y_VALUE.append(POINTS[i][1])
-    POINT_NUMBER.append(i)
-print(X_VALUE, Y_VALUE)
+   X_VALUE.append(POINTS[i][0])
+   Y_VALUE.append(POINTS[i][1])
+   #converts radians to degrees
+   PHI.append(degrees(POINTS[i][2]))
+   POINT_NUMBER.append(i)
 # Splits the point into an x and y component and adds it to 2 seperate lists
 # For each list inside the  POINTS list, divide them into thier x and y components
 # and add them to seperate lists. The POINT NUMBER list is the index of each point
 # to help with ordering
 
-plt.scatter(Y_VALUE, X_VALUE, color='red')
+#ignore this is just array of 1's
+U = V = np.ones_like(X_VALUE)
+
+ax.quiver(X_VALUE, Y_VALUE, U, V, angles=PHI,color='red')
+
+#plt.scatter(X_VALUE, Y_VALUE, color='red')
 for i, txt in enumerate(POINT_NUMBER):
-    plt.annotate(txt, (Y_VALUE[i], X_VALUE[i]), color='white')
+   plt.annotate(txt, (X_VALUE[i], Y_VALUE[i]), color='blue')
 plt.show()
 # This bit actually does the plotting
